d4a2256a19010bd5cccdb541baf9e12dcdc7ba99
fztfztfzt <782879714@qq.com>
2025-03-24 00:00:50 +0800

    引入NavMeshPlus和怪物移动功能

---
+++ Assets/AAAGame/Scripts/Components/NavigationComponent.cs
@@ -0,0 +1,12 @@
+﻿
+using NavMeshPlus.Components;
+using UnityGameFramework.Runtime;
+
+public class NavigationComponent : GameFrameworkComponent
+{
+    public NavMeshSurface Surface2D;
+    private void Update()
+    {
+        Surface2D.UpdateNavMesh(Surface2D.navMeshData);
+    }
+}
---
+++ Assets/AAAGame/Scripts/Entity/MonsterEntity.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterEntity : EntityBase
+{
+    public NavMeshAgent agent;
+    public Transform target;
+    private void Awake()
+    {
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+    private void Update()
+    {
+        if(target!= null)
+        {
+            agent.destination = target.position;
+        }
+    }
+}
