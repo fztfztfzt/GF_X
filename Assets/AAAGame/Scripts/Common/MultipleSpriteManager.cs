7ba9be0aeac375205572a63aadac7ef27ce7c2b3
fztfztfzt <782879714@qq.com>
2025-03-30 22:32:12 +0800

    add数据上传

---
+++ Assets/AAAGame/Scripts/Common/MultipleSpriteManager.cs
@@ -0,0 +1,65 @@
+﻿
+using Cysharp.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+public partial class MultipleSpriteManager
+{
+    private static MultipleSpriteManager m_Instance;
+    public static MultipleSpriteManager Instance
+    {
+        get
+        {
+            if (m_Instance == null)
+            {
+                m_Instance = new ();
+            }
+            return m_Instance;
+        }
+    }
+
+    private Dictionary<string, Sprite> m_SpriteDict = new ();
+    HashSet<string> m_LoadedSprite = new HashSet<string>();
+
+    public async UniTask LoadMultipleSprite(string path)
+    {
+        if (m_LoadedSprite.Contains(path))
+        {
+            return;
+        }
+        m_LoadedSprite.Add(path);
+        if (!Application.isPlaying)
+        {
+            var sprites = AssetDatabase.LoadAllAssetsAtPath(path).OfType<Sprite>().ToArray();
+            foreach (Sprite sprite in sprites)
+            {
+                m_SpriteDict.Add(sprite.name, sprite);
+            }
+            return;
+        }
+        var ans = await GF.Resource.LoadAssetAwait<MultipleSprite>(path);
+        OnLoadGFExtensionSuccess(ans);
+    }
+
+    private void OnLoadGFExtensionSuccess(object asset)
+    {
+        var spAtlas = asset as MultipleSprite;
+        foreach (var item in spAtlas.Sprites)
+        {
+            m_SpriteDict.Add(item.Key, item.Value);
+        }
+    }
+
+    public Sprite GetSprite(string name)
+    {
+        if (m_SpriteDict.ContainsKey(name))
+        {
+            return m_SpriteDict[name];
+        }
+        GF.LogError($"MultipleSpriteManager.GetSprite: Sprite {name} not found.");
+        return null;
+    }
+}
+
---
+++ Assets/AAAGame/Scripts/Entity/ItemEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class ItemEntity : EntityBase
+{
+    public SpriteRenderer spriteRenderer;
+    cfg.item gridItemInfo;
+    protected override void OnShow(object userData)
+    {
+        base.OnShow(userData);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        var data = Params.param as ItemGridData;
+        gridItemInfo = cfg.Tables.Instance.Tbitem[data.dataId];
+        UpdateShow();
+    }
+
+
+    async void UpdateShow()
+    {
+        await MultipleSpriteManager.Instance.LoadMultipleSprite(UtilityBuiltin.AssetsPath.GetSpritesPath(gridItemInfo.MultipleSprite));
+        spriteRenderer.sprite = MultipleSpriteManager.Instance.GetSprite(gridItemInfo.Image);
+    }
+
+    public bool Use()
+    {
+        if (gridItemInfo.Effect1.Length>0)
+        {
+            switch (gridItemInfo.Effect1.Length)
+            {
+                case 1:
+                    if(GF.Floor.PlayerEntity.IsFullHp) return false;
+                    GF.Floor.PlayerEntity.ApplyHeal(gridItemInfo.Param1[0]);
+                    break;
+            }
+        }
+        return true;
+    }
+
+}
